ff85bc73107e12cb4d9c300637029c03a90f3299
Devesh.Suryawanshi <deveshsuryawashi940@gmail.com>
2023-05-07 00:02:09 +0530

    Day-4

--- starfitness/src/Components/LandingPage/Landingpage.jsx
+++ starfitness/src/Components/LandingPage/Landingpage.jsx
@@ -7,8 +7,8 @@ import style from "./Landingpage.module.css"
 export default function Landingpage() {
 
     return (
-        <Box display={"flex"} flexDirection={"row"} justifyContent={"center"}>
-            <HStack className={style.hstack} width={"90%"} display={"flex"} justifyContent={"space-between"}>
+        <Box display={"flex"} flexDirection="column" alignItems={"center"}>
+            <Box className={style.hstack}>
                 <Box className={style.box1}>
                     <Text className={style.heading1}>Come Train With Us</Text>
                     <Text className={style.textContent1}>The StarFitness Training Club App helps you reach your fitness goals with expertly designed workouts from our world-class StarFitness Master Trainers. NTC is perfect for training at home, in the gym or on the road, with everything from bodyweight-only to full-equipment workouts for everyone at all fitness levels.</Text>
@@ -16,10 +16,21 @@ export default function Landingpage() {
                         <Link to="/Products">Get Exlusive Deals on fitness Products</Link>
                     </Box>
                 </Box>
-                <Box  display={"flex"} >
-                    <Img src='https://assets.gqindia.com/photos/61a320bc0cb715e3116b8655/16:9/pass/weekend%20workout.jpg' className={style.image1} borderRadius={"20px"} alt="Error" />
+                <Box display={"flex"} alignItems={"center"}>
+                    <Box>
+                        <Img src='https://assets.gqindia.com/photos/61a320bc0cb715e3116b8655/16:9/pass/weekend%20workout.jpg' className={style.image1} alt="Error" />
+                    </Box>
                 </Box>
-            </HStack>
+            </Box>
+            <Box className={style.hstack}>
+                <Box>
+                    <Img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ52H1_28mWY8rvZvOd75YzWRJmdhRlfnX3dg&usqp=CAU" />
+                </Box>
+                <Box>
+                    <Text className={style.heading1}>Come Train With Us</Text>
+                    <Text className={style.textContent1}>The StarFitness Training Club App helps you reach your fitness goals with expertly designed workouts from our world-class StarFitness Master Trainers. NTC is perfect for training at home, in the gym or on the road, with everything from bodyweight-only to full-equipment workouts for everyone at all fitness levels.</Text>
+                </Box>
+            </Box>
         </Box>
     )
 }   
